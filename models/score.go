eb7237f342aa2108d274bd90ad6dd5339b832897
Rob O'Dwyer <odwyerrob@gmail.com>
2017-09-04 10:21:59 -0700

    replace length scoring with inverse power function

--- models/score.go
+++ models/score.go
@@ -1,5 +1,7 @@
 package models
 
+import "math"
+
 // A Scorer is used to calculate a score for each result returned by the server.
 type Scorer interface {
 	Score(Location) float64
@@ -8,26 +10,19 @@ type Scorer interface {
 // A RelativeLengthScorer scores results based on the length of their names
 // relative to the length of the query. Longer names are given lower scores.
 type RelativeLengthScorer struct {
-	maxLength   int
 	queryLength int
 }
 
 func NewRelativeLengthScorer(locations []Location, query string) *RelativeLengthScorer {
-	// find maximum length in location names
-	maxLength := 1
-	for _, location := range locations {
-		length := len(location.Name)
-		if length > maxLength {
-			maxLength = length
-		}
-	}
-
 	return &RelativeLengthScorer{
 		queryLength: len(query),
-		maxLength:   maxLength,
 	}
 }
 
 func (scorer *RelativeLengthScorer) Score(location Location) float64 {
-	return 1.0 - (float64(len(location.Name)-scorer.queryLength) / float64(scorer.maxLength))
+	return inverseLengthScore(len(location.Name) - scorer.queryLength)
+}
+
+func inverseLengthScore(n int) float64 {
+	return math.Exp2(-float64(n))
 }
---
+++ models/score_test.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestRelativeLengthScorer_Score(t *testing.T) {
+	tests := map[string]struct {
+		query     string
+		locations []Location
+		location  Location
+		expected  float64
+	}{
+		"matching length": {
+			"ABC",
+			[]Location{{Name: "ABC"}},
+			Location{Name: "ABC"},
+			1.0,
+		},
+		"longer result": {
+			"ABC",
+			[]Location{{Name: "ABCD"}},
+			Location{Name: "ABCD"},
+			inverseLengthScore(1),
+		},
+		"empty string": {
+			"",
+			[]Location{{Name: ""}},
+			Location{Name: ""},
+			1.0,
+		},
+		"multiple results don't affect score": {
+			"",
+			[]Location{{Name: "ABC"}, {Name: "DEF"}, {Name: "GHI"}},
+			Location{Name: "DEF"},
+			inverseLengthScore(3),
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			scorer := NewRelativeLengthScorer(tt.locations, tt.query)
+
+			if actual := scorer.Score(tt.location); actual != tt.expected {
+				t.Errorf("%f", actual)
+				t.Errorf("%v != %v", actual, tt.expected)
+			}
+		})
+	}
+}
